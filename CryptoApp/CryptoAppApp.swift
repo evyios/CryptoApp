6018c3aa6a9e58e42fbb11f3a2dda12773c607f1
Evgeny Yachny <ev.yachny@gmail.com>
2022-04-10 15:42:31 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Evgeny on 10.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
